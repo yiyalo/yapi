ea19d2a5eb947d9858f79db2223a53fab6350c21
Tan Chee How <cheehow_tan@hotmail.com>
2018-05-27 19:22:05 +0800

    Update Model Relationship

--- app/Model/Product.php
+++ app/Model/Product.php
@@ -3,8 +3,13 @@
 namespace App\Model;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Model\Review;
 
 class Product extends Model
 {
-    //
+    //Relationship of Product and Review
+    public function reviews(){
+        return $this->hasMany(Review::class);
+    }
+
 }
--- app/Model/Review.php
+++ app/Model/Review.php
@@ -3,8 +3,12 @@
 namespace App\Model;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Model\Product;
 
 class Review extends Model
 {
-    //
+    //Relationship of Product and Review
+    public function product(){
+        return $this->belongsTo(Product::class);                //Which mean each review only belongs to ONLY ONE product! No one review can't belongs to multiple product.
+    }
 }
