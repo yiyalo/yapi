49dc3b3773a669381e6580d8a634388621b3310b
Tan Chee How <cheehow_tan@hotmail.com>
2018-06-03 17:20:58 +0800

    Product Update Included

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -7,9 +7,16 @@ use App\Model\Product;
 use Illuminate\Http\Request;
 use App\Http\Resources\Product\ProductResource;
 use App\Http\Resources\Product\ProductCollection;
+use App\Http\Requests\ProductRequest;
+use Symfony\Component\HttpFoundation\Response;
 
 class ProductController extends Controller
 {
+    public function __construct(){
+        $this->middleware('auth:api')->except('index', 'show');
+    }
+
+
     /**
      * Display a listing of the resource.
      *
@@ -51,9 +58,20 @@ class ProductController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(ProductRequest $request)
     {
-        //
+        //Create a new Product
+        $product = new Product;
+        $product->name = $request->name;
+        $product->detail = $request->description;
+        $product->year = $request->pyear;
+        $product->price = $request->price;
+        $product->discount = $request->discount;
+        $product->save();
+
+        return response([
+            'data' => new ProductResource($product)
+        ], Response::HTTP_CREATED);
     }
 
     /**
@@ -88,7 +106,11 @@ class ProductController extends Controller
      */
     public function update(Request $request, Product $product)
     {
-        //
+        //Update an existent product
+        $request['detail'] = $request->description;
+        $request['year'] = $request->pyear;
+
+        $product->update($request->all());
     }
 
     /**
--- app/Model/Product.php
+++ app/Model/Product.php
@@ -7,6 +7,11 @@ use App\Model\Review;
 
 class Product extends Model
 {
+    //To Make the value changeable or updateable
+    protected $fillable = [
+        'name', 'detail', 'year', ' price', 'discount'
+    ];
+
     //Relationship of Product and Review
     public function reviews(){
         return $this->hasMany(Review::class);
